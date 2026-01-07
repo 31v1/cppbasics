b0f43d2060517bd16db9cbe69606e39226b990fe
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-01-07 14:02:22 -0600

    Update calculator.cpp
    division by 0 and while fro option repeat

--- calculator.cpp
+++ calculator.cpp
@@ -5,7 +5,7 @@ int main() {
     int a;
     int b;
     char option;
-    float resultado;
+    float result;
 
     cout << "Enter the first number: " << endl;
     cin >> a;
@@ -18,28 +18,46 @@ int main() {
     cout << "2. Subtraction (-)" << endl;
     cout << "3. Multiplication (*)" << endl;
     cout << "4. Division (/)" << endl;
-
+    
     cin >> option;
+    
+    while ( option >= '1' && option <= '4') { 
+        
+        switch (option){
+            case '1':
+                        result = a+b;
+                        cout << "The addition result is: " <<result<<endl;
+            break;
+            case '2':
+                        result = a-b;
+                        cout << "The subtraction result is: " <<result<<endl;
+            break;
+            case '3':
+                        result = a*b;
+                        cout << "The multiplication result is: " <<result<<endl;                      
+            break;
+            case '4':
+                        if ( b == 0){
+                            cout << "You cannot divide by 0. "<<endl;
+                            cin >> b;
+                        } 
+                        result = (float)a/b;
+                        cout << "The division result is: " <<result<<endl;
+            break;
+            default:
+                        cout << "Choose an eligible option." <<endl;
+            break;
+        }
 
-    switch (option){
-        case '1':
-                    resultado = a+b;
-                    cout << "The addition result is: " <<resultado<<endl;
-        break;
-        case '2':
-                    resultado = a-b;
-                    cout << "The subtraction result is: " <<resultado<<endl;
-        break;
-        case '3':
-                    resultado = a*b;
-                    cout << "The multiplication result is: " <<resultado<<endl;
-        break;
-        case '4':
-                    resultado = a/b;
-                    cout << "The division result is: " <<resultado<<endl;
-        break;
-        default:
-                    cout << "Choose an eligible option." <<endl;
-        break;
-}
+        // Menú repetido dentro del while 
+        cout << "\nChoose an operation from the following: " << endl; 
+        cout << "1. Addition (+)" << endl; 
+        cout << "2. Subtraction (-)" << endl; 
+        cout << "3. Multiplication (*)" << endl; 
+        cout << "4. Division (/)" << endl; 
+        cout << "Press any other key to exit." << endl;
+        
+        cin >> option;
+    }
+    cout << "Exiting calculator." << endl;
 }
