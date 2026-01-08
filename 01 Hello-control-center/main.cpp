b2b1ef3ccd4b35cdd17c19b781f3f4337968358e
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-01-07 18:47:57 -0600

    Add Hello Control Center exercise
    Add welcome message for cosmonauts in main function

--- 01 Hello-control-center/main.cpp
+++ 01 Hello-control-center/main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
 
+using namespace std;
+
+int main () {
+
+cout << "Welcome cosmonauts" <<endl;
+cout << "Hope you have rested well through the night, cosmonauts." << endl;
+return 0;
+
+}
