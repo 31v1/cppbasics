b9dcd559528fe52c130456422c36e98807af6dec
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-01-07 19:20:33 -0600

    Delete main function for piece serial number check
    Removed the main function that checks piece serial number.

--- ndustrial Piece Scanner/main.cpp
+++ ndustrial Piece Scanner/main.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
 
-using namespace std;
-
-int main () {
-
-int num;
-
-  cout << "Enter the piece serial number" << endl;
-  cin >> num;
-  
-  if ((num % 2) == 0) {
-    cout << "Accepted" << endl;
-  } else {
-    cout << "Rejected, odd number" << endl;
-  }
-  return 0;
-}
