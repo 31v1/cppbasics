75e8ffd4fb7e17d8e4a7b595c3f945fdc97933b5
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-01-07 20:50:28 -0600

    Add counter display program in main.cpp

---
+++ 06 Counter Display/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+int number;
+int counter = 1;
+
+cout << "Enter a positive number" << endl;
+cin >> number;
+
+while (counter <= number) {
+  cout << counter << endl;
+  counter++;
+}
+return 0;
+}
+
+
+ 
+
+
