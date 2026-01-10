c87a3236f21b86595499b72d148ede77098bf777
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-01-10 00:46:11 -0600

    Create main.cpp

---
+++ 09 Machine Startup Counter/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  int target;
+
+  cout << "Enter the target number for the startup sequence" << endl;
+  cin >> target;
+
+  for (int i = 1; i <= target; i++){
+    cout << i << endl;
+  }
+
+return 0;
+}
