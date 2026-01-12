92e719355a9aeb7cea8a171fb3cba231da6368a5
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-01-11 18:58:11 -0600

    Update main.cpp

--- 10 Polar Operations Check/main.cpp
+++ 10 Polar Operations Check/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+using namespace std:
+
+int main () {
+  int number;
+  int oddNumber;
+  int evenNumber;
+
+  cout << "Enter the numeric sequence." << endl;
+  cin >> number;
+
+  if number 
